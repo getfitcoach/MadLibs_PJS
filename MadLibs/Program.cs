5ce1f2bbd0dfd3b0407a8d740fbd915c077a3177
Paul <gowild45@gmail.com>
2020-01-06 11:59:59 -0500

    first commit after reset

---
+++ MadLibs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //identify the start point of all curly braces in string
+            string template = "{noun} ate a {food} with his/her {body part}";
+            int open = 0;
+            int close = 0;
+
+            for (i = 0; i )
+            //read the characters between the curly brackets 
+            //ask the person to provide a {word}
+            //replace the {word} with the persons {word}
+            //do this for all {words}
+            //print string.template with the new {words}
+
+
+
+            
+        }
+        
+        
+
+    }
+   
+}
